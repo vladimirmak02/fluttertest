b1fd9ff10432e2694ec5455bfa731d607801a845
vladimirmak02 <vovamak107@gmail.com>
2020-02-27 19:53:30 +0100

    Initial commit Adding existing project files for the test project

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home()
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My test app'),
+        centerTitle: true,
+        backgroundColor: Colors.green[600],
+      ),
+      body: Center(
+        child: Text(
+          'wow',
+          style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.black38,
+              fontFamily: 'Oxanium'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('+'),
+        backgroundColor: Colors.green[600],
+      ),
+    );
+  }
+}

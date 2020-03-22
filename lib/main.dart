9867de17836902bcf7c3eb4dc7ad437ea5fb13d2
Vladimir <vovamak107@gmail.com>
2020-03-22 16:12:16 +0100

    completed tutorial 21 on quotes and cards and stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'quote.dart';
 
@@ -11,11 +12,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List<Quote> alltexts = [
-    Quote(text: 'Egor idiot',author: 'Vlad'),
-    Quote(text: 'Mr pyle is devil',author: 'Egor'),
-    Quote(text: 'School is boring',author: 'Everyone'),
+    Quote(text: 'Egor idiot', author: 'Vlad'),
+    Quote(text: 'Mr pyle is devil', author: 'Egor'),
+    Quote(text: 'School is boring', author: 'Everyone'),
   ];
 
   @override
@@ -28,15 +28,18 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.redAccent,
       ),
       body: Container(
-        child: Center(
-          child: Column(
-            children: alltexts.map((tempText){
-              return Text('${tempText.text} - ${tempText.author}');
-            }).toList(),
-          ),
+        child: Column(
+          children: alltexts
+              .map((tempText) => QuoteCard(
+                  text: tempText,
+                  delete: () {
+                    setState(() {
+                      alltexts.remove(tempText);
+                    });
+                  }))
+              .toList(),
         ),
       ),
-
     );
   }
 }

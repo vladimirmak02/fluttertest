84368c464fb48711812b612f09e3927958c169d6
Vladimir <vovamak107@gmail.com>
2020-03-24 19:05:04 +0100

    started worldclock program

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'quote.dart';
+import 'package:flutter_testapp/pages/home.dart';
+import 'package:flutter_testapp/pages/change_location.dart';
+import 'package:flutter_testapp/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
       home: Home(),
     ));
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  List<Quote> alltexts = [
-    Quote(text: 'Egor idiot', author: 'Vlad'),
-    Quote(text: 'Mr pyle is devil', author: 'Egor'),
-    Quote(text: 'School is boring', author: 'Everyone'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("Texts"),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Container(
-        child: Column(
-          children: alltexts
-              .map((tempText) => QuoteCard(
-                  text: tempText,
-                  delete: () {
-                    setState(() {
-                      alltexts.remove(tempText);
-                    });
-                  }))
-              .toList(),
-        ),
-      ),
-    );
-  }
-}
